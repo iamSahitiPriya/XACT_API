164410d6fce9d78cba61be14fb25773dc59af025
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-08-29 16:25:57 +0530

    [102] | [Sahiti] Refactor path for api

---
+++ src/main/resources/db/migration/V22__alter-constraint.sql
@@ -0,0 +1,8 @@
+-- ALTER TABLE tbm_assessment_category ADD CONSTRAINT category_name_unq UNIQUE (category_name);
+-- ALTER TABLE tbm_assessment_module ADD CONSTRAINT module_name_unq UNIQUE (module_name);
+-- ALTER TABLE tbm_assessment_topic ADD CONSTRAINT topic_name_unq UNIQUE (topic_name);
+-- ALTER TABLE tbm_assessment_parameter ADD CONSTRAINT parameter_name_unq UNIQUE (parameter_name);
+--
+--
+--
+--

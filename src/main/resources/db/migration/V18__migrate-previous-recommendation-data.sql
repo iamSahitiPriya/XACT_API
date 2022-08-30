fb5b98a5f44d14a196c2ce522464c767767a00fc
Shashank Mishra <shashank.mishra@thoughtworks.com>
2022-08-30 16:28:42 +0530

    [No-Card] [Shashank] | Correct sql script order

---
+++ src/main/resources/db/migration/V18__migrate-previous-recommendation-data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO tbl_assessment_topic_recommendation (assessment, topic,recommendation,created_at,updated_at) SELECT
+                                                                                                             assessment_id,topic_id,recommendation,created_at,updated_at FROM tbl_assessment_topic;
+
+INSERT INTO tbl_assessment_parameter_recommendation (assessment,parameter,recommendation,created_at,updated_at) SELECT
+                                                                                                                    assessment_id,parameter_id,recommendation,created_at,updated_at FROM tbl_assessment_parameter;
+
+
+ALTER TABLE "tbl_assessment_topic" DROP "recommendation";
+
+ALTER TABLE "tbl_assessment_parameter" DROP "recommendation";

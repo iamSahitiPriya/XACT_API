972bc5f1e82b04f7ea426cdb2ce98b9db862012a
RUCHIKA MODGIL <ruchika.modgil@thoughtworks.com>
2022-09-15 10:27:03 +0530

    [125] [Ruchika] Refactor Report Controller

--- src/main/java/com/xact/assessment/controllers/ReportController.java
+++ src/main/java/com/xact/assessment/controllers/ReportController.java
@@ -21,6 +21,7 @@ import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -80,19 +81,22 @@ public class ReportController {
         ReportDataResponse reportDataResponse = new ReportDataResponse();
         reportDataResponse.setName(assessment.getAssessmentName());
         List<ReportCategoryResponse> reportCategoryResponseList  = new ArrayList<>();
+        mapToResponseStructure(assessmentCategories, reportCategoryResponseList);
+        reportDataResponse.setChildren(reportCategoryResponseList);
+
+        return HttpResponse.ok(reportDataResponse);
+
+    }
+
+    private void mapToResponseStructure(List<AssessmentCategory> assessmentCategories, List<ReportCategoryResponse> reportCategoryResponseList) {
         for(AssessmentCategory assessmentCategory: assessmentCategories){
-            ReportCategoryResponse reportCategoryResponse = new ReportCategoryResponse();
-            reportCategoryResponse.setName(assessmentCategory.getCategoryName());
-            reportCategoryResponse.setRating(assessmentCategory.getCategoryAverage());
+            ReportCategoryResponse reportCategoryResponse = getReportCategoryResponse(assessmentCategory);
             List<ReportModuleResponse> reportModuleResponseList = new ArrayList<>();
             for(AssessmentModule assessmentModule: assessmentCategory.getModules()){
-                ReportModuleResponse reportModuleResponse = new ReportModuleResponse();
-                reportModuleResponse.setName(assessmentModule.getModuleName());
-                reportModuleResponse.setRating(assessmentModule.getModuleAverage());
+                ReportModuleResponse reportModuleResponse = getReportModuleResponse(assessmentModule);
                 List<ReportTopicResponse> reportTopicResponseList = new ArrayList<>();
                 for(AssessmentTopic assessmentTopic: assessmentModule.getTopics()){
-                    ReportTopicResponse reportTopicResponse = new ReportTopicResponse();
-                    reportTopicResponse.setName(assessmentTopic.getTopicName());
+                    ReportTopicResponse reportTopicResponse = getReportTopicResponse(assessmentTopic);
                     if(assessmentTopic.hasReferences()){
                         reportTopicResponse.setValue(assessmentTopic.getRating());
                     }
@@ -100,9 +104,7 @@ public class ReportController {
                         reportTopicResponse.setRating(assessmentTopic.getTopicAverage());
                         List<ReportParameterResponse> reportParameterResponseList = new ArrayList<>();
                         for(AssessmentParameter assessmentParameter : assessmentTopic.getParameters()){
-                            ReportParameterResponse reportParameterResponse = new ReportParameterResponse();
-                            reportParameterResponse.setName(assessmentParameter.getParameterName());
-                            reportParameterResponse.setValue(assessmentParameter.getRating());
+                            ReportParameterResponse reportParameterResponse = getReportParameterResponse(assessmentParameter);
                             reportParameterResponseList.add(reportParameterResponse);
                         }
                         reportTopicResponse.setChildren(reportParameterResponseList);
@@ -116,11 +118,38 @@ public class ReportController {
             reportCategoryResponseList.add(reportCategoryResponse);
 
         }
-        reportDataResponse.setChildren(reportCategoryResponseList);
-
-        return HttpResponse.ok(reportDataResponse);
-
     }
 
+    @NotNull
+    private ReportParameterResponse getReportParameterResponse(AssessmentParameter assessmentParameter) {
+        ReportParameterResponse reportParameterResponse = new ReportParameterResponse();
+        reportParameterResponse.setName(assessmentParameter.getParameterName());
+        reportParameterResponse.setValue(assessmentParameter.getRating());
+        return reportParameterResponse;
     }
+
+    @NotNull
+    private ReportTopicResponse getReportTopicResponse(AssessmentTopic assessmentTopic) {
+        ReportTopicResponse reportTopicResponse = new ReportTopicResponse();
+        reportTopicResponse.setName(assessmentTopic.getTopicName());
+        return reportTopicResponse;
+    }
+
+    @NotNull
+    private ReportModuleResponse getReportModuleResponse(AssessmentModule assessmentModule) {
+        ReportModuleResponse reportModuleResponse = new ReportModuleResponse();
+        reportModuleResponse.setName(assessmentModule.getModuleName());
+        reportModuleResponse.setRating(assessmentModule.getModuleAverage());
+        return reportModuleResponse;
+    }
+
+    @NotNull
+    private ReportCategoryResponse getReportCategoryResponse(AssessmentCategory assessmentCategory) {
+        ReportCategoryResponse reportCategoryResponse = new ReportCategoryResponse();
+        reportCategoryResponse.setName(assessmentCategory.getCategoryName());
+        reportCategoryResponse.setRating(assessmentCategory.getCategoryAverage());
+        return reportCategoryResponse;
+    }
+
+}
 

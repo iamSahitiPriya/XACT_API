64fa5d9e77cfd72927731db3522e7e952383b56c
Shashank Mishra <shashank.mishra@thoughtworks.com>
2022-09-19 19:07:15 +0530

    [No-Card] [Shashank] | Refactor code to use project reactor and remove spring annotations

--- src/main/java/com/xact/assessment/repositories/AccessControlRepository.java
+++ src/main/java/com/xact/assessment/repositories/AccessControlRepository.java
@@ -8,19 +8,15 @@ import com.xact.assessment.models.AccessControlList;
 import com.xact.assessment.models.AccessControlRoles;
 import io.micronaut.context.annotation.Executable;
 import io.micronaut.data.annotation.Query;
+import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @Repository
-public interface AccessControlRepository extends CrudRepository<AccessControlList,String> {
+public interface AccessControlRepository extends CrudRepository<AccessControlList, String> {
     @Executable
     @Query("SELECT acl.accessControlRoles FROM AccessControlList acl WHERE acl.email=:email")
     Optional<AccessControlRoles> getAccessControlRolesByEmail(String email);
-
-    @Executable
-    boolean existsByEmail(@NotNull String email);
 
 }

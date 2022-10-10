38b0ccf1eb2e7bf02bbc6acde5732c05d0c2d2a1
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2022-10-10 14:00:26 +0530

    [156]|[Sahiti|Ram] Add test cases for assessment models

---
+++ src/test/java/unit/com/xact/assessment/models/AssessmentTest.java
@@ -0,0 +1,64 @@
+package unit.com.xact.assessment.models;
+
+import com.xact.assessment.models.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+
+class AssessmentTest {
+
+    @Test
+    void shouldCheckForAssessmentStatus() {
+        String userEmail = "dummy@test.com";
+        Assessment assessment = new Assessment();
+        AssessmentUsers assessmentUsers = new AssessmentUsers();
+        Organisation organisation = new Organisation();
+        organisation.setSize(5);
+        organisation.setIndustry("new");
+        organisation.setDomain("new");
+        organisation.setOrganisationName("testorg");
+        assessment.setAssessmentName("mocked assessment");
+        assessment.setAssessmentPurpose("Client Request");
+        assessment.setAssessmentStatus(AssessmentStatus.Completed);
+        assessment.setOrganisation(organisation);
+        UserId userId = new UserId(userEmail, assessment);
+        assessmentUsers.setUserId(userId);
+        assessmentUsers.setRole(AssessmentRole.Owner);
+
+        boolean actualResponse = assessment.isEditable();
+        Assertions.assertFalse(actualResponse);
+    }
+
+    @Test
+    void shouldCheckForAssessmentState() {
+        String userEmail = "dummytest@test.com";
+        Assessment assessment = new Assessment();
+        AssessmentUsers assessmentUsers = new AssessmentUsers();
+        Organisation organisation = new Organisation();
+        organisation.setSize(9);
+        organisation.setIndustry("industry");
+        organisation.setDomain("domain");
+        organisation.setOrganisationName("organisation");
+        assessment.setAssessmentName("assessmentName");
+        assessment.setAssessmentPurpose("Client Request");
+        assessment.setAssessmentStatus(AssessmentStatus.Active);
+        assessment.setOrganisation(organisation);
+        AssessmentCategory category = new AssessmentCategory("name",true,"");
+        category.setCategoryId(1);
+        AssessmentModule module = new AssessmentModule("module",category,true,"");
+        AssessmentModuleId assessmentModuleId = new AssessmentModuleId(assessment,module);
+        UserAssessmentModule userAssessmentModule = new UserAssessmentModule(assessmentModuleId,assessment,module);
+        Set<UserAssessmentModule> assessmentModules= Collections.singleton(userAssessmentModule);
+        assessment.setAssessmentModules(assessmentModules);
+        UserId userId = new UserId(userEmail, assessment);
+        assessmentUsers.setUserId(userId);
+        assessmentUsers.setRole(AssessmentRole.Owner);
+
+        boolean actualCategoryStatus = assessment.getCategoryStatus();
+        Assertions.assertTrue(actualCategoryStatus);
+    }
+}

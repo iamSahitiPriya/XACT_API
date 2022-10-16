02c4dced10c516d51153732ba962b6f1fd43b51b
rampg23 <ramprakash.pg@thoughtworks.com>
2022-10-16 20:59:45 +0530

    [157][Ram|Brindha] | Create Account table.

---
+++ src/main/java/com/xact/assessment/models/Accounts.java
@@ -0,0 +1,32 @@
+package com.xact.assessment.models;
+
+import io.micronaut.core.annotation.Introspected;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Introspected
+@Entity
+@Table(name = "tbm_accounts")
+public class Accounts {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "industry", nullable = false)
+    private String industry;
+}

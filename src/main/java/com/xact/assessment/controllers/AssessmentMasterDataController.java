b83d180ea2678f4758439f93b3d547651ec3ef1a
Shashank Mishra <shashank.mishra@thoughtworks.com>
2022-12-07 19:16:40 +0530

    [164] Shashank | Refactor report mapper

--- src/main/java/com/xact/assessment/controllers/AssessmentMasterDataController.java
+++ src/main/java/com/xact/assessment/controllers/AssessmentMasterDataController.java
@@ -4,8 +4,11 @@
 
 package com.xact.assessment.controllers;
 
-import com.xact.assessment.dtos.*;
-import com.xact.assessment.models.*;
+import com.xact.assessment.dtos.AssessmentCategoryDto;
+import com.xact.assessment.dtos.CategoryDto;
+import com.xact.assessment.dtos.UserAssessmentResponse;
+import com.xact.assessment.mappers.MasterDataMapper;
+import com.xact.assessment.models.AssessmentCategory;
 import com.xact.assessment.services.AssessmentMasterDataService;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.HttpResponse;
@@ -16,15 +19,12 @@ import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.TreeSet;
 
 @Introspected
 @Controller("/v1/assessment-master-data")
@@ -32,55 +32,7 @@ public class AssessmentMasterDataController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AssessmentMasterDataController.class);
 
 
-    private static final ModelMapper moduleMapper = new ModelMapper();
-    private static final ModelMapper mapper = new ModelMapper();
-
-    static {
-        PropertyMap<AssessmentModule, AssessmentModuleDto> moduleMapOnly = new PropertyMap<>() {
-            protected void configure() {
-                map().setCategory(source.getCategory().getCategoryId());
-                map().setTopics(new TreeSet<>());
-            }
-        };
-        PropertyMap<AssessmentModule, AssessmentModuleDto> moduleMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setCategory(source.getCategory().getCategoryId());
-            }
-        };
-        PropertyMap<AssessmentTopic, AssessmentTopicDto> topicMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setModule(source.getModule().getModuleId());
-            }
-        };
-        PropertyMap<AssessmentParameter, AssessmentParameterDto> parameterMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setTopic(source.getTopic().getTopicId());
-            }
-        };
-        PropertyMap<Question, QuestionDto> questionMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setParameter(source.getParameter().getParameterId());
-            }
-        };
-        PropertyMap<AssessmentTopicReference, AssessmentTopicReferenceDto> topicReferenceMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setTopic(source.getTopic().getTopicId());
-            }
-        };
-        PropertyMap<AssessmentParameterReference, AssessmentParameterReferenceDto> parameterReferenceMap = new PropertyMap<>() {
-            protected void configure() {
-                map().setParameter(source.getParameter().getParameterId());
-            }
-        };
-        mapper.addMappings(moduleMap);
-        mapper.addMappings(topicMap);
-        mapper.addMappings(parameterMap);
-        mapper.addMappings(questionMap);
-        mapper.addMappings(topicReferenceMap);
-        mapper.addMappings(parameterReferenceMap);
-        moduleMapper.addMappings(moduleMapOnly);
-    }
-
+    private final MasterDataMapper masterDataMapper = new MasterDataMapper();
     private final AssessmentMasterDataService assessmentMasterDataService;
 
     public AssessmentMasterDataController(AssessmentMasterDataService assessmentMasterDataService) {
@@ -95,12 +47,13 @@ public class AssessmentMasterDataController {
         List<AssessmentCategory> userAssessmentCategories = assessmentMasterDataService.getUserAssessmentCategories(assessmentId);
         List<AssessmentCategoryDto> userAssessmentCategoriesResponse = new ArrayList<>();
         if (Objects.nonNull(userAssessmentCategories)) {
-            userAssessmentCategories.forEach(assessmentCategory -> userAssessmentCategoriesResponse.add(moduleMapper.map(assessmentCategory, AssessmentCategoryDto.class)));
+            userAssessmentCategories.forEach(assessmentCategory -> userAssessmentCategoriesResponse.add(masterDataMapper.mapTillModuleOnly(assessmentCategory)));
+
         }
         List<AssessmentCategory> assessmentCategories = assessmentMasterDataService.getAllCategories();
         List<AssessmentCategoryDto> assessmentCategoriesResponse = new ArrayList<>();
         if (Objects.nonNull(assessmentCategories)) {
-            assessmentCategories.forEach(assessmentCategory -> assessmentCategoriesResponse.add(moduleMapper.map(assessmentCategory, AssessmentCategoryDto.class)));
+            assessmentCategories.forEach(assessmentCategory -> assessmentCategoriesResponse.add(masterDataMapper.mapTillModuleOnly(assessmentCategory)));
         }
         UserAssessmentResponse userAssessmentResponse = new UserAssessmentResponse();
         userAssessmentResponse.setAssessmentCategories(assessmentCategoriesResponse);
@@ -115,7 +68,7 @@ public class AssessmentMasterDataController {
         List<AssessmentCategory> userAssessmentCategories = assessmentMasterDataService.getUserAssessmentCategories(assessmentId);
         List<AssessmentCategoryDto> userAssessmentCategoriesResponse = new ArrayList<>();
         if (Objects.nonNull(userAssessmentCategories)) {
-            userAssessmentCategories.forEach(assessmentCategory -> userAssessmentCategoriesResponse.add(mapper.map(assessmentCategory, AssessmentCategoryDto.class)));
+            userAssessmentCategories.forEach(assessmentCategory -> userAssessmentCategoriesResponse.add(masterDataMapper.map(assessmentCategory)));
         }
         UserAssessmentResponse userAssessmentResponse = new UserAssessmentResponse();
         userAssessmentResponse.setUserAssessmentCategories(userAssessmentCategoriesResponse);
@@ -129,7 +82,7 @@ public class AssessmentMasterDataController {
         List<AssessmentCategory> assessmentCategories = assessmentMasterDataService.getCategories();
         List<CategoryDto> assessmentCategoriesResponse = new ArrayList<>();
         if (Objects.nonNull(assessmentCategories)) {
-            assessmentCategories.forEach(assessmentCategory -> assessmentCategoriesResponse.add(mapper.map(assessmentCategory, CategoryDto.class)));
+            assessmentCategories.forEach(assessmentCategory -> assessmentCategoriesResponse.add(masterDataMapper.mapCategory(assessmentCategory)));
         }
         return HttpResponse.ok(assessmentCategoriesResponse);
     }

e6036e22c8a978e18cb1f87cd41d50ce572dbd42
Shashank Mishra <shashank.mishra@thoughtworks.com>
2022-12-21 13:21:48 +0530

    [214] Shashank | Correct sorting logic and remove toLowercase in getters and setters

--- src/main/java/com/xact/assessment/dtos/AssessmentRequest.java
+++ src/main/java/com/xact/assessment/dtos/AssessmentRequest.java
@@ -31,23 +31,6 @@ public class AssessmentRequest {
     @NotBlank
     private String domain;
 
-    public void setAssessmentName(String assessmentName) {
-        this.assessmentName = assessmentName.toLowerCase();
-    }
-
-    public void setOrganisationName(String organisationName) {
-        this.organisationName = organisationName.toLowerCase();
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain.toLowerCase();
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry.toLowerCase();
-    }
-
-
     @NotBlank
     private String industry;
     @Min(1)

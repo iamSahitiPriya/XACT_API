0ba3a87d3fc2d2c52c2793f0da505e3fd9b3851f
Shashank Mishra <shashank.mishra@thoughtworks.com>
2022-12-22 18:59:25 +0530

    [No-Card] Shashank | Improve global error handling

--- src/main/java/com/xact/assessment/exceptions/GlobalErrorResponseProcessor.java
+++ src/main/java/com/xact/assessment/exceptions/GlobalErrorResponseProcessor.java
@@ -6,15 +6,22 @@ package com.xact.assessment.exceptions;
 
 
 import io.micronaut.context.annotation.Replaces;
+import io.micronaut.http.HttpMethod;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
+import io.micronaut.http.hateoas.Resource;
+import io.micronaut.http.server.exceptions.response.Error;
 import io.micronaut.http.server.exceptions.response.ErrorContext;
 import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
 import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Produces
 @Singleton
@@ -23,9 +30,31 @@ public class GlobalErrorResponseProcessor implements ErrorResponseProcessor<Json
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalErrorResponseProcessor.class);
 
     @Override
-    public MutableHttpResponse<JsonError> processResponse(ErrorContext errorContext, MutableHttpResponse<?> baseResponse) {
+    public MutableHttpResponse<JsonError> processResponse(ErrorContext errorContext, MutableHttpResponse<?> response) {
         LOGGER.error("URI: {}", errorContext.getRequest().getUri());
-        LOGGER.error("Exception: {}", errorContext.getRootCause());
-        return HttpResponse.status(baseResponse.status());
+
+        if (errorContext.getRequest().getMethod() == HttpMethod.HEAD) {
+            return (MutableHttpResponse<JsonError>) response;
+        }
+        JsonError error;
+        if (!errorContext.hasErrors()) {
+            error = new JsonError(response.getStatus().getReason());
+        } else if (errorContext.getErrors().size() == 1) {
+            Error jsonError = errorContext.getErrors().get(0);
+            error = new JsonError(jsonError.getMessage());
+            jsonError.getPath().ifPresent(error::path);
+        } else {
+            error = new JsonError(response.getStatus().getReason());
+            List<Resource> errors = new ArrayList<>();
+            for (Error jsonError : errorContext.getErrors()) {
+                errors.add(new JsonError(jsonError.getMessage()).path(jsonError.getPath().orElse(null)));
+            }
+            error.embedded("errors", errors);
+        }
+        error.link(Link.SELF, Link.of(errorContext.getRequest().getUri()));
+
+        LOGGER.error("Exception log: {}", error);
+
+        return HttpResponse.status(response.status());
     }
 }

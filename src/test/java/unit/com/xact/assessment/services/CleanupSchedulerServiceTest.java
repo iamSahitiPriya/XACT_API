e09b5354f6c9ea3b820c6af34b774124e14ce074
@brindha.e <brindha.e@thoughtworkworks.com>
2023-01-24 14:17:52 +0530

    [266][Brindha | Ram]Add. clean up activity logs

--- src/test/java/unit/com/xact/assessment/services/CleanupSchedulerServiceTest.java
+++ src/test/java/unit/com/xact/assessment/services/CleanupSchedulerServiceTest.java
@@ -1,5 +1,6 @@
 package unit.com.xact.assessment.services;
 
+import com.xact.assessment.repositories.ActivityLogRepository;
 import com.xact.assessment.repositories.NotificationRepository;
 import com.xact.assessment.services.CleanupSchedulerService;
 import org.junit.jupiter.api.Test;
@@ -7,15 +8,17 @@ import org.junit.jupiter.api.Test;
 import java.util.Date;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class CleanupSchedulerServiceTest {
 
     NotificationRepository notificationRepository = mock(NotificationRepository.class);
+    ActivityLogRepository activityLogRepository = mock(ActivityLogRepository.class);
     CleanupSchedulerService cleanupSchedulerService;
 
     public CleanupSchedulerServiceTest() {
-       this.cleanupSchedulerService = new CleanupSchedulerService(notificationRepository);
+       this.cleanupSchedulerService = new CleanupSchedulerService(notificationRepository, activityLogRepository);
     }
 
     @Test
@@ -23,5 +26,12 @@ class CleanupSchedulerServiceTest {
         cleanupSchedulerService.cleanSentNotifications();
 
         verify(notificationRepository).deleteSentNotifications(any(Date.class),any(Date.class));
+    }
+
+    @Test
+    void shouldDeleteExpiredActivityLogs() {
+        cleanupSchedulerService.cleanExpiredActivityLogs();
+
+        verify(activityLogRepository).deleteActivityLogs(any(Date.class),any(Date.class));
     }
 }

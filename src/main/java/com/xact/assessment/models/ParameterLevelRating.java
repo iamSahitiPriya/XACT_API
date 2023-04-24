4e5efbf85865d8b71f90046d3978f564f1abbcdd
@brindha.e <brindha.e@thoughtworkworks.com>
2023-04-24 17:10:47 +0530

    [231][Brindha]Remove. code smells

--- src/main/java/com/xact/assessment/models/ParameterLevelRating.java
+++ src/main/java/com/xact/assessment/models/ParameterLevelRating.java
@@ -9,8 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;

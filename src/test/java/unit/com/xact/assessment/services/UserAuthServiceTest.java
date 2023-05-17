33a8ca756c2a16ee42c175ef8486d879d27df6d3
rampg23 <ramprakash.pg@thoughtworks.com>
2023-05-17 15:23:53 +0530

    [408][Ram] Fix test coverage for services

--- src/test/java/unit/com/xact/assessment/services/UserAuthServiceTest.java
+++ src/test/java/unit/com/xact/assessment/services/UserAuthServiceTest.java
@@ -5,32 +5,36 @@
 package unit.com.xact.assessment.services;
 
 import com.xact.assessment.client.UserInfoClient;
+import com.xact.assessment.dtos.AccessControlRoleDto;
 import com.xact.assessment.dtos.UserInfoDto;
+import com.xact.assessment.exceptions.UnauthorisedUserException;
+import com.xact.assessment.models.AccessControlRoles;
 import com.xact.assessment.models.User;
 import com.xact.assessment.models.UserInfo;
 import com.xact.assessment.repositories.UserRepository;
+import com.xact.assessment.services.AccessControlService;
 import com.xact.assessment.services.ModuleContributorService;
 import com.xact.assessment.services.UserAuthService;
 import io.micronaut.security.authentication.Authentication;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class UserAuthServiceTest {
 
 
-    private Authentication authentication = mock(Authentication.class);
-    private UserInfoClient userInfoClient = mock(UserInfoClient.class);
-    private UserRepository userRepository = mock(UserRepository.class);
-    private ModuleContributorService moduleContributorService = mock(ModuleContributorService.class);
-    private UserAuthService userAuthService = new UserAuthService(userInfoClient, userRepository, moduleContributorService, accessControlService);
+    private final Authentication authentication = mock(Authentication.class);
+    private final UserInfoClient userInfoClient = mock(UserInfoClient.class);
+    private final UserRepository userRepository = mock(UserRepository.class);
+    AccessControlService accessControlService = Mockito.mock(AccessControlService.class);
+
+    private final ModuleContributorService moduleContributorService = mock(ModuleContributorService.class);
+    private final UserAuthService userAuthService = new UserAuthService(userInfoClient, userRepository, moduleContributorService, accessControlService);
 
 
     @Test
@@ -102,7 +106,67 @@ class UserAuthServiceTest {
         userAuthService.login("some header", currentUser);
 
         assertEquals("dummy@thoughtworks.com", currentUser.getUserEmail());
-        verify(userRepository,times(0)).save(userInfo);
+        verify(userRepository, times(0)).save(userInfo);
+    }
+
+    @Test
+    void shouldGetLoggedInUsers() {
+        Set<String> users = new HashSet<>();
+        users.add("abc@thoughtworks.com");
+        UserInfo userInfo = new UserInfo();
+        userInfo.setEmail("abc@thoughtworks.com");
+        userInfo.setFirstName("ABC");
+        userInfo.setLastName("DEF");
+        userInfo.setLocale("US");
+        when(userRepository.findByUsers(users)).thenReturn(Collections.singletonList(userInfo));
+
+        List<UserInfo> userInfos = userAuthService.getLoggedInUsers(users);
+
+        assertEquals(userInfos.size(), 1);
+    }
+
+    @Test
+    void shouldGetAllUsers() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setEmail("abc@thoughtworks.com");
+        userInfo.setFirstName("ABC");
+        userInfo.setLastName("DEF");
+        userInfo.setLocale("US");
+        when(userRepository.findAllUsers()).thenReturn(Collections.singletonList(userInfo));
+
+        List<UserInfo> userInfos = userAuthService.getUsers();
+        assertEquals(userInfos.size(), 1);
+    }
+
+    @Test
+    void shouldValidateUser() {
+        String email = "dummy@thoughtworks.com";
+        Map<String, Object> authMap = new HashMap<>();
+        authMap.put("sub", email);
+        User currentUser = new User();
+        currentUser.setUserInfo(new UserInfo(email, null, null, null));
+
+        when(authentication.getAttributes()).thenReturn(authMap);
+        when(userRepository.findById(email)).thenReturn(Optional.empty());
+        when(accessControlService.getAccessControlRolesByEmail(email)).thenReturn(Optional.of(AccessControlRoles.PRIMARY_ADMIN));
+
+        userAuthService.validateUser(authentication, AccessControlRoles.PRIMARY_ADMIN);
+        verify(accessControlService).getAccessControlRolesByEmail(email);
+    }
+
+    @Test
+    void shouldThrowExceptionIfTheUserIsInvalid() {
+        String email = "dummy@thoughtworks.com";
+        Map<String, Object> authMap = new HashMap<>();
+        authMap.put("sub", email);
+        User currentUser = new User();
+        currentUser.setUserInfo(new UserInfo(email, null, null, null));
+
+        when(authentication.getAttributes()).thenReturn(authMap);
+        when(userRepository.findById(email)).thenReturn(Optional.empty());
+        when(accessControlService.getAccessControlRolesByEmail(email)).thenReturn(Optional.of(AccessControlRoles.SECONDARY_ADMIN));
+
+        assertThrows(UnauthorisedUserException.class, () -> userAuthService.validateUser(authentication, AccessControlRoles.PRIMARY_ADMIN));
     }
 
 }

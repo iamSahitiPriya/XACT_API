00f0e7a0a4cd50e13591d60ef4b16ac59b82dbac
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2023-05-22 15:57:30 +0530

    merge with develop

--- src/main/resources/db/migration/V40__alter-topic-and-parameter-table.sql
+++ src/main/resources/db/migration/V40__alter-topic-and-parameter-table.sql
@@ -9,8 +9,4 @@ alter table tbm_assessment_parameter add column has_reference bool default  fals
 update tbm_assessment_topic as topic set has_reference = true where topic.topic_id in (select distinct(topic) from tbm_assessment_topic_reference);
 update tbm_assessment_parameter as parameter set has_reference = true where parameter.parameter_id in (select distinct(parameter) from tbm_assessment_param_reference);
 
-alter table tbm_assessment_question_reference drop constraint tbm_assessment_question_reference_question_fkey;
-
-alter table tbm_assessment_question_reference add constraint tbm_assessment_question_reference_fkey foreign key (question) references tbm_assessment_question (question_id)
-    on delete cascade
 
--- src/main/resources/db/migration/V41__create-question-level-reference.sql
+++ src/main/resources/db/migration/V41__create-question-level-reference.sql
@@ -9,3 +9,7 @@ create table tbm_assessment_question_reference (
                                                    reference text
 );
 alter table tbl_assessment_question add column score int;
+alter table tbm_assessment_question_reference drop constraint tbm_assessment_question_reference_question_fkey;
+
+alter table tbm_assessment_question_reference add constraint tbm_assessment_question_reference_fkey foreign key (question) references tbm_assessment_question (question_id)
+    on delete cascade
